3ce32fa1f7d896d4d8a2e6bed6fd6d567351d0e2
Pedro <pedroneubert22@hotmail.com>
2024-04-22 21:44:28 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+var setaDireita =window.document.getElementById("setadireita")
+var leonardoo = window.document.getElementById("lenardoo")
+var samantha = window.document.getElementById("samantha")
+var Bruna = window.document.getElementById("Bruna")
+var setaEsquerda = window.document.getElementById("setaesquerda")
+
+function RolarParaDireita() {
+    leonardo.style ="display:none"
+    Bruna.style ="display:flex"
+    setaDireita.style ="display:none"
+    setaEsquerda.style ="display:flex" 
+}
+
+function RolarParaEsquerda() {
+    leonardo.style ="display:flex"
+    Bruna.style ="display:none"
+    setaDireita.style ="display:flex"
+    setaEsquerda.style ="display:none"
+}
+
